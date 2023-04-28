6573d38c31873611e1875bc3bbdd9e0cdafd73e9
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2023-04-28 17:13:25 +0300

    переробив скролл

--- src/js/listeners/onScrollEndOfPage.js
+++ src/js/listeners/onScrollEndOfPage.js
@@ -5,18 +5,20 @@ import { instanceApiService } from "../api-service";
 
 export async function onScrollEndOfPage(e) {
   if(window.innerHeight + window.scrollY >= document.documentElement.scrollHeight &&  window.scrollY !== 0) {
-    if(instanceApiService.totalElements < 40) {
-        return Notify.info("We're sorry, but you've reached the end of search results.");
-    }
+    
+    if(instanceApiService.totalElementsOnPage < 40) {
+          return Notify.info("We're sorry, but you've reached the end of search results.");
+      }
 
-  try {
-    const objectResolve = await instanceApiService.fetchPhoto();
-    const { hits } = objectResolve;
+    try {
+      const objectResolve = await instanceApiService.fetchPhoto();
+      const { hits } = objectResolve;
 
-    instanceApiService.totalElements = hits.length;
-    renderPhotos(hits, e)
-    } catch(error) {
-      Notify.failure('Что-то пошло не так');
-    } 
+      instanceApiService.totalElementsOnPage = hits.length;
+      renderPhotos(hits, e)
+      } catch(error) {
+        Notify.failure('Что-то пошло не так');
+        console.log(error.message);
+      } 
 }
 };
